ff7ca5dea9abb5b9e41f9b47bfa6b98b76d88b08
Gabriele Broccoli <gabriele.broccoli@gmail.com>
2023-12-20 18:05:12 +0100

    prime modifiche

---
+++ app.js
@@ -0,0 +1,27 @@
+const Home = {
+    data() {
+        return {
+            //variabili
+        }
+    },
+
+    methods: {
+        //funzioni
+    },
+
+    template: `` //template
+}
+
+const Prenota = {
+    data() {
+        return {
+            //variabili
+        }
+    },
+
+    methods: {
+        //funzioni
+    },
+
+    template: `` //template
+}
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const http = require("http");
+const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
 app.get("/", function(req, res){
+    res.status(404).send("Ops, pagina non trovata");
     res.sendFile(__dirname + "/index.html");
 });
 
