d50c2548f99d4f96407ba9ae144e8626c7af37e6
Gabroz4 <gabriele.broccoli@gmail.com>
2023-12-27 15:09:56 +0100

    css homepage

--- progetto_bs_2.0/frontend/src/pages/Home.vue
+++ progetto_bs_2.0/frontend/src/pages/Home.vue
@@ -24,6 +24,8 @@ export default defineComponent({
 <template>
   <h2>Home</h2>
   <img :src="'/img/' + 'home.jpg'" alt="" />
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
   <article v-for="camera in datiCamere">
     <h3>{{camera.nomecamera}}</h3>
     <img :src="'/img/' + camera.imgcamera1" alt="" />   
