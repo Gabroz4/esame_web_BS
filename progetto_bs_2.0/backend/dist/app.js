7281dacd3af29d9411687870482cfd204ea0211a
Gabroz4 <gabriele.broccoli@gmail.com>
2023-12-28 17:37:48 +0100

    primo login funzionante

--- progetto_bs_2.0/backend/dist/app.js
+++ progetto_bs_2.0/backend/dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,35 +15,66 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const camere_router_1 = __importDefault(require("./routes/camere-router"));
-/*import categorieRouter from "./routes/categorie-router";
-import autoriRouter from "./routes/autori-router";*/
-const connect_history_api_fallback_1 = __importDefault(require("connect-history-api-fallback"));
+const db_1 = require("./utils/db");
+const crypto_1 = require("crypto");
+// Creazione di un'app Express
 const app = (0, express_1.default)();
+// Porta su cui il server ascolterà
 const port = 3000;
-app.use((0, connect_history_api_fallback_1.default)());
-app.use(express_1.default.static("public"));
-app.use(express_1.default.static("dist-frontend"));
+// Middleware per la gestione delle route storiche
+app.use(require('connect-history-api-fallback')());
+// Utilizzo di middleware per il parsing dei dati JSON e URL-encoded
+app.use(express_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+// Middleware per la gestione delle route statiche
+app.use(express_1.default.static('public'));
+app.use(express_1.default.static('dist-frontend'));
 // Usa bodyParser per analizzare il corpo delle richieste POST
 app.use(body_parser_1.default.json());
+//stampa di tutte le camere nel db
 app.use(camere_router_1.default);
-/*app.use(categorieRouter);
-app.use(autoriRouter);*/
-// Aggiungi il tuo endpoint di login qui
-app.post('/api/login', (req, res) => {
+// Endpoint di login
+app.post('/api/login', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    // Estrazione di email e password dalla richiesta
     const { email, password } = req.body;
-    // Qui dovresti verificare l'email e la password con il tuo database
-    // Per ora, useremo un controllo fittizio
-    if (email === 'test@example.com' && password === 'password') {
-        res.json({ success: true });
+    try {
+        // Prevenzione dell'invio del modulo se i campi sono vuoti
+        if (!email || !password) {
+            return res.json({ success: false, message: 'Per favore, inserisci sia l\'email che la password' });
+        }
+        // Query per cercare l'utente nel database
+        const query = 'SELECT email,password FROM utente WHERE email = ? AND password = ?';
+        var hashedPassword = (0, crypto_1.createHash)('sha512').update(password).digest('hex');
+        hashedPassword = hashedPassword.substring(0, 45);
+        // Esecuzione della query nel database
+        const [results] = yield db_1.connection.promise().query(query, [email, hashedPassword]);
+        console.log('Risultati:', results);
+        // Verifica se ci sono risultati nella query
+        if (Array.isArray(results) && results.length > 0) {
+            // Utilizzo del tipo più preciso per results[0]
+            const user = results[0];
+            if (user) {
+                res.json({ success: true, message: 'Login riuscito' });
+            }
+            else {
+                res.json({ success: false, message: 'Credenziali non valide' });
+            }
+        }
+        else {
+            res.json({ success: false, message: 'Credenziali non valide' });
+        }
     }
-    else {
-        res.json({ success: false });
+    catch (err) {
+        console.error('Errore nella query al database:', err);
+        res.json({ success: false, message: 'Errore nel login' });
     }
+}));
+// Gestione della risposta per le pagine non trovate
+app.use((req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Ops... Pagina non trovata');
 });
-app.use(function (req, res, next) {
-    res.setHeader("Content-Type", "text/plain");
-    res.status(404).send("Ops... Pagina non trovata");
-});
+// Avvio del server sulla porta specificata
 app.listen(port, function () {
     console.log(`Listening on http://localhost:${port}`);
 });
--- progetto_bs_2.0/backend/src/app.ts
+++ progetto_bs_2.0/backend/src/app.ts
@@ -1,42 +1,78 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import camereRouter from "./routes/camere-router";
-/*import categorieRouter from "./routes/categorie-router";
-import autoriRouter from "./routes/autori-router";*/
 import history from "connect-history-api-fallback";
+import { connection } from './utils/db';
+import { createHash } from 'crypto';
 
+// Creazione di un'app Express
 const app: Express = express();
+// Porta su cui il server ascolterà
 const port: number = 3000;
 
-app.use(history());
-app.use(express.static("public"));
-app.use(express.static("dist-frontend"));
+// Middleware per la gestione delle route storiche
+app.use(require('connect-history-api-fallback')());
+
+// Utilizzo di middleware per il parsing dei dati JSON e URL-encoded
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware per la gestione delle route statiche
+app.use(express.static('public'));
+app.use(express.static('dist-frontend'));
 
 // Usa bodyParser per analizzare il corpo delle richieste POST
 app.use(bodyParser.json());
 
+
+//stampa di tutte le camere nel db
 app.use(camereRouter);
-/*app.use(categorieRouter);
-app.use(autoriRouter);*/
 
-// Aggiungi il tuo endpoint di login qui
-app.post('/api/login', (req, res) => {
-  const { email, password } = req.body;
+// Endpoint di login
+app.post('/api/login', async (req: Request, res: Response) => {
+  // Estrazione di email e password dalla richiesta
+  const { email, password }: { email: string; password: string } = req.body;
 
-  // Qui dovresti verificare l'email e la password con il tuo database
-  // Per ora, useremo un controllo fittizio
-  if (email === 'test@example.com' && password === 'password') {
-    res.json({ success: true });
-  } else {
-    res.json({ success: false });
+  try {
+    // Prevenzione dell'invio del modulo se i campi sono vuoti
+    if (!email || !password) {
+      return res.json({ success: false, message: 'Per favore, inserisci sia l\'email che la password' });
+    }
+
+    // Query per cercare l'utente nel database
+    const query = 'SELECT email,password FROM utente WHERE email = ? AND password = ?';
+    var hashedPassword = createHash('sha512').update(password).digest('hex');
+    hashedPassword = hashedPassword.substring(0, 45);
+
+    // Esecuzione della query nel database
+    const [results] = await connection.promise().query(query, [email, hashedPassword]);
+    console.log('Risultati:', results);
+
+    // Verifica se ci sono risultati nella query
+    if (Array.isArray(results) && results.length > 0) {
+      // Utilizzo del tipo più preciso per results[0]
+      const user: Record<string, unknown> = results[0] as Record<string, unknown>;
+      if (user) {
+        res.json({ success: true, message: 'Login riuscito' });
+      } else {
+        res.json({ success: false, message: 'Credenziali non valide' });
+      }
+    } else {
+      res.json({ success: false, message: 'Credenziali non valide' });
+    }
+  } catch (err) {
+    console.error('Errore nella query al database:', err);
+    res.json({ success: false, message: 'Errore nel login' });
   }
 });
 
-app.use(function(req, res, next) {
-  res.setHeader("Content-Type", "text/plain");
-  res.status(404).send("Ops... Pagina non trovata");
+// Gestione della risposta per le pagine non trovate
+app.use((req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.status(404).send('Ops... Pagina non trovata');
 });
 
+// Avvio del server sulla porta specificata
 app.listen(port, function() {
   console.log(`Listening on http://localhost:${port}`);
 });
